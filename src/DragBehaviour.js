e1250dff9901cbf36cdb72383a3b00c9b01713c4
Liu Shuo <liu@shuo.email>
2017-04-07 15:56:39 +0800

    refactor

--- src/DragBehaviour.js
+++ src/DragBehaviour.js
@@ -3,11 +3,7 @@ module.exports = function () {
   let dragBehaviour = d3.drag()
 
   dragBehaviour.on('start', function () {
-    // let cursor = d3.mouse(document.querySelector('#ForceGraph'))
-    // let sourceEvent = d3.event.sourceEvent
-    // let nodePosition = sourceEvent.target.parentNode.getCTM()
-    //
-    // if (sourceEvent.shiftKey) scope.addLink({x1: nodePosition.e, y1: nodePosition.f, x2: cursor[0], y2: cursor[1]})
+    scope.relationIterator ++
   })
 
   dragBehaviour.on('drag', function () {
@@ -19,19 +15,15 @@ module.exports = function () {
     }
 
     if (sourceEvent.shiftKey) {
-      if (!scope.previewRelation) scope.previewRelation = `relation-${scope.relationIterator ++}`
-
+      let relation = `relation-${scope.relationIterator}`
       let origin = this.parentNode.getCTM()
       let cursor = d3.mouse(document.querySelector('#ForceGraph'))
-      scope.previewLink({relation: scope.previewRelation, x1: origin.e, y1: origin.f, x2: cursor[0], y2: cursor[1]})
+      scope.previewLink({relation, x1: origin.e, y1: origin.f, x2: cursor[0], y2: cursor[1]})
     }
   })
 
   dragBehaviour.on('end', function () {
-    if (!scope.targetNode) {
-      scope.removeLink(scope.previewRelation)
-      scope.previewRelation = null
-    }
+    if (!scope.targetNode) scope.removeLink(`relation-${scope.relationIterator}`)
   })
 
   return dragBehaviour
