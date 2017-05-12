4360d0a97e86542dd89797566b0386b38620dcc9
Liu Shuo <liu@shuo.email>
2017-05-12 17:53:17 +0800

    bind link instance to DOM element

--- src/Link.js
+++ src/Link.js
@@ -1,72 +1,45 @@
-function edit (d, i, g) {
-  console.log('edit', g[i].parentNode)
-  let handle = d3.select(g[i].parentNode).select('g.bezierHandle')
-  let display = handle.attr('display')
-  if (display === 'none') handle.attr('display', 'block')
-  if (display === 'block') handle.attr('display', 'none')
-}
-
-function circle (className, position) {
-  let circle = document.createElementNS(d3.namespaces.svg, 'circle')
-  d3.select(circle).attr('r', 5).attr('class', className)
-  .attr('cx', position[0])
-  .attr('cy', position[1])
-
-  return circle
-}
-
-function bezierHandle (from, to) {
-  function moveHandle (selection) {
-    let dragBehaviour = d3.drag()
-    dragBehaviour.on('drag', (d, i, g) => {
-      let cursor = d3.mouse(document.querySelector('svg'))
-      console.log('drag handle', cursor)
-      d3.select(g[i]).attr('cx', cursor[0]).attr('cy', cursor[1])
-      // TODO: updates pathDescription
-    })
-
-    selection.call(dragBehaviour)
+class Link {
+  constructor (id) {
+    this.id = id
   }
 
-  let group = document.createElementNS(d3.namespaces.svg, 'g')
-  d3.select(group).attr('class', 'bezierHandle').attr('display', 'none')
+  controlHandle () {
+    let tick = [(this.to[0] - this.from[0]) / 6, (this.to[1] - this.from[1]) / 6]
+    this.controlFrom = [this.from[0] + tick[0], this.from[1] + tick[1]]
+    this.controlTo = [this.to[0] - tick[0], this.to[1] - tick[1]]
+  }
 
-  let tick = [(to[0] - from[0]) / 6, (to[1] - from[1]) / 6]
-  let controlFrom = [from[0] + tick[0], from[1] + tick[1]]
-  d3.select(group).append(() => circle('controlFrom', controlFrom)).call(moveHandle)
-  let controlTo = [to[0] - tick[0], to[1] - tick[1]]
-  d3.select(group).append(() => circle('controlTo', controlTo)).call(moveHandle)
+  path () {
+    let path = document.createElementNS(d3.namespaces.svg, 'path')
+    d3.select(path)
+    .attr('class', 'path')
+    .attr('d', this.pathDescription())
+  //  .on('dblclick', edit)
 
-  return group
-}
+    return path
+  }
 
-function path (from, to) {
-  let pathDescription = d3.path()
+  SVGElement (origin) {
+    this.from = origin
+    this.to = origin
+    this.controlHandle()
 
-  let tick = [(to[0] - from[0]) / 6, (to[1] - from[1]) / 6]
-  let controlFrom = [from[0] + tick[0], from[1] + tick[1]]
-  let controlTo = [to[0] - tick[0], to[1] - tick[1]]
+    let group = document.createElementNS(d3.namespaces.svg, 'g')
+    d3.select(group).attr('class', 'links').attr('id', this.id)
+    d3.select(group).append(() => this.path())
 
-  pathDescription.moveTo(from[0], from[1])
-  pathDescription.bezierCurveTo(controlFrom[0], controlFrom[1], controlTo[0], controlTo[1], to[0], to[1])
+    return group
+  }
 
-  let path = document.createElementNS(d3.namespaces.svg, 'path')
-  d3.select(path)
-  .attr('class', 'path')
-  .attr('d', pathDescription.toString())
-  .on('dblclick', edit)
+  pathDescription (waypoints, calculateHandle) {
+    for (let key in waypoints) { this[key] = waypoints[key] }
+    if (calculateHandle) this.controlHandle()
+    let pathDescription = d3.path()
+    pathDescription.moveTo(this.from[0], this.from[1])
+    pathDescription.bezierCurveTo(this.controlFrom[0], this.controlFrom[1], this.controlTo[0], this.controlTo[1], this.to[0], this.to[1])
 
-  return path
-}
-
-function Link (from, to) {
-  let group = document.createElementNS(d3.namespaces.svg, 'g')
-  d3.select(group).attr('class', 'links')
-
-  d3.select(group).append(() => path(from, to))
-  d3.select(group).append(() => bezierHandle(from, to))
-
-  return group
+    return pathDescription.toString()
+  }
 }
 
 module.exports = Link
--- src/SVGCanvas.js
+++ src/SVGCanvas.js
@@ -3,7 +3,7 @@ let Link = require('./Link.js')
 class SVGCanvas extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = { iterator: 0 }
   }
 
   setGraphSize () {
@@ -27,9 +27,6 @@ class SVGCanvas extends React.Component {
   //   }
   // }
 
-  // TODO: drawPath (from, to, [controlFrom, controlTo])
-  // return pathDescription
-
   componentDidMount () {
     this.setGraphSize()
     window.onresize = this.setGraphSize
@@ -37,31 +34,18 @@ class SVGCanvas extends React.Component {
     let dragBehaviour = d3.drag()
     dragBehaviour.on('start', (d, i, g) => {
       let cursor = d3.mouse(document.querySelector('svg'))
-      let linkData = ['test']
-      d3.select('svg').selectAll('g.links')
-      .data(linkData)
-      .enter()
-      .append(() => Link(cursor, cursor))
 
-      this.setState({from: cursor})
+      let link = new Link(`test-${++this.state.iterator}`)
+      d3.select('svg').selectAll('g.links')
+      .data([link], (d, i, g) => d.id)
+      .enter()
+      .append((d) => d.SVGElement(cursor))
     })
     dragBehaviour.on('drag', (d, i, g) => {
-      let pathDescription = d3.path()
-      let from = this.state.from
-      let to = d3.mouse(document.querySelector('svg'))
-      let tick = [(to[0] - from[0]) / 6, (to[1] - from[1]) / 6]
-      let controlFrom = [from[0] + tick[0], from[1] + tick[1]]
-      let controlTo = [to[0] - tick[0], to[1] - tick[1]]
-      pathDescription.moveTo(from[0], from[1])
-      pathDescription.bezierCurveTo(controlFrom[0], controlFrom[1], controlTo[0], controlTo[1], to[0], to[1])
-
-      console.log(pathDescription.toString())
-      let linkData = ['test']
-      let link = d3.select('svg').selectAll('g.links').data(linkData)
-
-      link.select('path').attr('d', pathDescription.toString())
-      link.select('circle.controlFrom').attr('cx', controlFrom[0]).attr('cy', controlFrom[1])
-      link.select('circle.controlTo').attr('cx', controlTo[0]).attr('cy', controlTo[1])
+      let cursor = d3.mouse(document.querySelector('svg'))
+      console.log(cursor, this.state.iterator)
+      d3.selectAll('svg g.links').filter((d, i, g) => (d.id === `test-${this.state.iterator}`))
+      .select('path').attr('d', (d) => d.pathDescription({to: cursor}, true))
     })
 
     d3.select('svg').call(dragBehaviour)
